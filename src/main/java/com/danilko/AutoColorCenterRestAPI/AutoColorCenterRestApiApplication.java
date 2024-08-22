d6bbbc87cc4842f7ec331c765f75da60930daea1
Danil <danil.r.savchenko@gmail.com>
2024-08-22 11:18:50 +0300

    Initialize Spring Boot Project

---
+++ src/main/java/com/danilko/AutoColorCenterRestAPI/AutoColorCenterRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.danilko.AutoColorCenterRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoColorCenterRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoColorCenterRestApiApplication.class, args);
+	}
+
+}
